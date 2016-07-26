2d0855f6dd7115d93f748c426d0bb8264ac89ef7
abhinavmsra <makeawish880@gmail.com>
2016-07-26 17:32:21 +0545

    checking..

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module GoogleScraper
       #{config.root}/app/uploaders
       #{config.root}/app/datatables/concerns
     )
-    config.server_static_assets= true
+    config.public_file_server.enabled = true
     # Enable the asset pipeline
     config.assets.enabled = true
   end
